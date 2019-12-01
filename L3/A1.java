9e35c1f854145cd06dd633345ac4e6237828d26e
thehardwareguy2000 <54811767+thehardwareguy2000@users.noreply.github.com>
2019-12-01 22:19:55 +0530

    Add files via upload
    Assignment of L3

---
+++ L3/A1.java
@@ -0,0 +1,14 @@
+class A1{
+		public static void main(String ... args){
+		byte b1= 2;
+		byte b2= 3;
+		byte b3= (byte)(b1+b2);
+		//int b3= b1+b2; (method2)
+		System.out.println("result="+b3);
+
+		}
+
+
+
+
+}
---
+++ L3/A2.java
@@ -0,0 +1,15 @@
+class A2{
+		public static void main(String ... args){
+		
+			//final int x=5;
+			int x=5;
+			int y=x++;
+			System.out.println("x="+x+"y="+y);
+
+
+	}
+}
+
+
+
+
