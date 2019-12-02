e7c242cb3b9f4fec82e4d117dfed46e23566609e
thehardwareguy2000 <54811767+thehardwareguy2000@users.noreply.github.com>
2019-12-02 22:04:22 +0530

    h.w.  files uploaded

---
+++ L4/H1.java
@@ -0,0 +1,9 @@
+class H1{
+	public static void main(String ... args){
+	int y=1;
+	do{
+	System.out.print(y++ +"");
+	}while(y<=10);
+	}
+
+}
---
+++ L4/H2.java
@@ -0,0 +1,10 @@
+class H2{
+public static void main(String ... args){
+	for(int i=1;i<=3;i++){
+		char c='a';
+		for(int j=1;j<=3;j++){
+			System.out.print(c++);
+		}
+	}
+ }
+}
